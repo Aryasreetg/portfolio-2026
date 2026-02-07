7cbb327aa955f9050f5778a2d14054b5e54a8e45
ARYASREE <aryasree@ARYASREEs-MacBook-Air.local>
2026-02-07 23:15:41 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 className="text-3xl font-bold text-gray-800">Welcome to the Home Page</h1>
+    
+   </div>
+  );
+}
